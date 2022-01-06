0627ac8cc25361925c805f20625c9e0cedae5d27
sanchez <dfitz360@gmail.com>
2022-01-06 18:51:50 +1000

    This should work, need more powr!

--- Sanchez.AOC.Challenges.2021/Challenges/Day22.cs
+++ Sanchez.AOC.Challenges.2021/Challenges/Day22.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Sanchez.AOC.Core;
@@ -10,8 +11,14 @@ namespace Sanchez.AOC.Challenges._2021.Challenges
     public class Day22 : ISolution
     {
         record Position(int X, int Y, int Z);
-        record OperationRange(int Min, int Max);
-        record Operation(bool OP, OperationRange X, OperationRange Y, OperationRange Z);
+        record OperationRange(int Min, int Max)
+        {
+            public bool IsInRange(int x) => x >= Min && x <= Max;
+        };
+        record Operation(bool OP, OperationRange X, OperationRange Y, OperationRange Z)
+        {
+            public bool IsInRange(Position p) => X.IsInRange(p.X) && Y.IsInRange(p.Y) && Z.IsInRange(p.Z);
+        };
 
         public string Part1()
         {
@@ -37,28 +44,77 @@ namespace Sanchez.AOC.Challenges._2021.Challenges
                 })
                 .ToList();
 
-            var reactor = new Dictionary<Position, bool>();
+            var lightsOn = 0;
+            for (var x = -50; x <= 50; x++)
+                for (var y = -50; y <= 50; y++)
+                    for (var z = -50; z <= 50; z++)
+                    {
+                        var pos = new Position(x, y, z);
+                        var operation = input
+                            .Where(x => x.IsInRange(pos))
+                            .LastOrDefault();
 
-            foreach (var line in input)
-                for (var x = line.X.Min; x <= line.X.Max; x++)
-                    for (var y = line.Y.Min; y <= line.Y.Max; y++)
-                        for (var z = line.Z.Min; z <= line.Z.Max; z++)
-                            reactor[new(x, y, z)] = line.OP;
+                        if (operation == null)
+                            continue;
 
-            var onCount = reactor
-                .Where(x =>
-                    x.Key.X >= -50 && x.Key.X <= 50 &&
-                    x.Key.Y >= -50 && x.Key.Y <= 50 &&
-                    x.Key.Z >= -50 && x.Key.Z <= 50)
-                .Where(x => x.Value)
-                .Count();
+                        if (operation.OP)
+                            lightsOn++;
+                    }
 
-            return onCount.ToString();
+            return lightsOn.ToString();
         }
 
         public string Part2()
         {
-            return "";
+            var input =
+                InputLoader.Load()
+                .NewLinedInput()
+                .Select(x =>
+                {
+                    var match = Regex.Match(x, @"^(\w+) x=(-?\d+)\.\.(-?\d+),y=(-?\d+)\.\.(-?\d+),z=(-?\d+)\.\.(-?\d+)$");
+
+                    var op = match.Groups[1].Value == "on";
+                    var xRange = new OperationRange(
+                        int.Parse(match.Groups[2].Value),
+                        int.Parse(match.Groups[3].Value));
+                    var yRange = new OperationRange(
+                        int.Parse(match.Groups[4].Value),
+                        int.Parse(match.Groups[5].Value));
+                    var zRange = new OperationRange(
+                        int.Parse(match.Groups[6].Value),
+                        int.Parse(match.Groups[7].Value));
+
+                    return new Operation(op, xRange, yRange, zRange);
+                })
+                .Reverse()
+                .ToList();
+
+            var minX = input.Select(x => x.X.Min).Min();
+            var maxX = input.Select(x => x.X.Max).Max();
+            var minY = input.Select(x => x.Y.Min).Min();
+            var maxY = input.Select(x => x.Y.Max).Max();
+            var minZ = input.Select(x => x.Z.Min).Min();
+            var maxZ = input.Select(x => x.Z.Max).Max();
+
+            long lightsOn = 0;
+            for (var x = minX; x <= maxX; x++)
+                for (var y = minY; y <= maxY; y++)
+                    for (var z = minZ; z <= maxZ; z++)
+                    {
+                        var pos = new Position(x, y, z);
+
+                        var operation = input
+                            .Where(x => x.IsInRange(pos))
+                            .FirstOrDefault();
+
+                        if (operation == null)
+                            continue;
+
+                        if (operation.OP)
+                            lightsOn++;
+                    }
+
+            return lightsOn.ToString();
         }
     }
 }
