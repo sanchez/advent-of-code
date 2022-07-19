67bd5048e3a576738b496692f9d3c472ad6ab5f3
sanchez <dfitz360@gmail.com>
2022-07-19 18:07:17 +1000

    Icons are now working as expected

---
+++ Sanchez.AOC/Converters/StatusToIconConverter.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia.Data.Converters;
+
+using Material.Icons;
+
+using Sanchez.AOC.Enums;
+
+using System.Globalization;
+
+namespace Sanchez.AOC.Converters
+{
+    public class StatusToIconConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            return (SolutionStatus)value switch
+            {
+                SolutionStatus.FAILED => MaterialIconKind.ProgressClose,
+                SolutionStatus.NOT_STARTED => MaterialIconKind.ProgressClock,
+                SolutionStatus.PARTIAL_FAIL => MaterialIconKind.ProgressAlert,
+                SolutionStatus.RUNNING => MaterialIconKind.ProgressHelper,
+                SolutionStatus.SUCCESS => MaterialIconKind.ProgressCheck,
+            };
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
