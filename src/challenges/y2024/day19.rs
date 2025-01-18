e11415daf3a4ed090837045e7acdd27eadf51cd9
sanchez <daniel.github@pm.me>
2025-01-18 23:07:31 +1100

    stubbed out the files

---
+++ src/challenges/y2024/day19.rs
@@ -0,0 +1,26 @@
+use crate::get_input;
+use itertools::Itertools;
+
+fn read_input() {}
+
+pub fn parta() -> usize {
+    0
+}
+pub fn partb() -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parta() {
+        assert_eq!(0, parta());
+    }
+
+    #[test]
+    fn test_partb() {
+        assert_eq!(0, partb());
+    }
+}
