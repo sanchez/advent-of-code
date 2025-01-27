df924f55f69f96269ad9098bc68aaaefdf4f1c4b
sanchez <daniel.github@pm.me>
2025-01-27 22:41:03 +1100

    I am not smart enough

--- src/challenges/y2024/day13.rs
+++ src/challenges/y2024/day13.rs
@@ -1,80 +1,123 @@
 use crate::get_input;
 use itertools::Itertools;
 use regex::Regex;
-use std::sync::mpsc;
+use std::{
+    ops::{Add, Mul},
+    sync::mpsc,
+};
 use threadpool::ThreadPool;
 
-fn calculate_distance(pos: (u64, u64)) -> u64 {
-    pos.0.pow(2) + pos.1.pow(2)
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct Position {
+    x: u64,
+    y: u64,
 }
 
-fn efficiency(pos: (u64, u64), cost: u64) -> f64 {
-    let distance = calculate_distance(pos);
-    distance as f64 / cost as f64
+impl Position {
+    fn new(x: u64, y: u64) -> Position {
+        Position { x, y }
+    }
+}
+
+impl Add<Position> for Position {
+    type Output = Position;
+
+    fn add(self, other: Position) -> Position {
+        Position {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl Mul<u64> for Position {
+    type Output = Position;
+
+    fn mul(self, other: u64) -> Position {
+        Position {
+            x: self.x * other,
+            y: self.y * other,
+        }
+    }
 }
 
 struct Machine {
-    a: (u64, u64),
-    a_efficency: f64,
-    b: (u64, u64),
-    b_efficency: f64,
-    prize: (u64, u64),
+    a: Position,
+    b: Position,
+    prize: Position,
 }
 
 impl Machine {
-    /// Calculates the cost for getting to the prize
-    fn calculate(
-        &self,
-        current_pos: (u64, u64),
-        depth: u64,
-        a_count: usize,
-        b_count: usize,
-    ) -> Option<u64> {
-        if a_count > 100 || b_count > 100 {
+    fn calculate(&self) -> Option<u64> {
+        let max_a = self.a * 100;
+        let max_b = self.b * 100;
+        let total_max = max_a + max_b;
+
+        // Early break to see if it's even possible at the max
+        if total_max.x < self.prize.x || total_max.y < self.prize.y {
             return None;
         }
 
-        println!("Got to depth: {}. A: {}, B: {}", depth, a_count, b_count);
-        // if depth > 100 {
-        //     return None;
-        // }
+        let max_a_steps = u64::max(self.prize.x / self.a.x, self.prize.y / self.a.y) + 2;
+        let max_a_steps = u64::min(max_a_steps, 100);
+        let max_b_steps = u64::max(self.prize.x / self.b.x, self.prize.y / self.b.y) + 2;
+        let max_b_steps = u64::min(max_b_steps, 100);
 
-        if current_pos == self.prize {
-            println!("We found a match for {:?}", self.prize);
-            return Some(0);
-        }
+        let best = (0..=max_a_steps)
+            .flat_map(|a| (0..=max_b_steps).map(move |b| (a, b)))
+            .filter_map(|(a, b)| {
+                let a_pos = self.a * a;
+                let b_pos = self.b * b;
+                let total = a_pos + b_pos;
 
-        if current_pos.0 > self.prize.0 && current_pos.1 > self.prize.1 {
-            // We over shot the prize
-            return None;
-        }
-
-        let a_pos = (current_pos.0 + self.a.0, current_pos.1 + self.a.1);
-        let b_pos = (current_pos.0 + self.b.0, current_pos.1 + self.b.1);
-
-        let mut test_order = vec!['a', 'b'];
-
-        if self.b_efficency < self.a_efficency {
-            test_order.swap(0, 1);
-        }
-
-        for x in test_order {
-            match x {
-                'a' => {
-                    if let Some(result) = self.calculate(a_pos, depth + 1, a_count + 1, b_count) {
-                        return Some(result + 3);
-                    }
+                if total == self.prize {
+                    return Some(a * 3 + b);
                 }
-                'b' => {
-                    if let Some(result) = self.calculate(b_pos, depth + 1, a_count, b_count + 1) {
-                        return Some(result + 1);
-                    }
-                }
-                _ => (),
+
+                None
+            })
+            .min();
+
+        best
+    }
+
+    fn get_position(&self, a: u64, b: u64) -> Position {
+        // println!("Getting position: {:?} {:?}", a, b);
+        let a_pos = self.a * a;
+        let b_pos = self.b * b;
+        a_pos + b_pos
+    }
+
+    fn calculate2(&self) -> Option<u64> {
+        println!("Starting: {:?}", self.prize);
+        let mut a = u64::min(self.prize.x / self.a.x, self.prize.y / self.a.y) - 2;
+        loop {
+            let pos = self.get_position(a, 0);
+            if pos.x >= self.prize.x || pos.y >= self.prize.y {
+                break;
             }
+
+            a += 1;
         }
 
-        None
+        let mut b = 0;
+        loop {
+            let mut pos = self.get_position(a, b);
+            if pos == self.prize {
+                return Some(a * 3 + b);
+            }
+
+            while pos.x > self.prize.x || pos.y > self.prize.y {
+                if a == 0 {
+                    return None;
+                }
+
+                a -= 1;
+                pos = self.get_position(a, b);
+            }
+
+            b += 1;
+        }
     }
 }
 
@@ -82,53 +125,83 @@ fn read_input() -> Vec<Machine> {
     let button_a = Regex::new(r"Button A: X\+(\d+), Y\+(\d+)\n").unwrap();
     let button_b = Regex::new(r"Button B: X\+(\d+), Y\+(\d+)\n").unwrap();
     let prizes = Regex::new(r"Prize: X=(\d+), Y=(\d+)\n").unwrap();
-    let input = get_input(2024, 13);
+    let mut input = get_input(2024, 13);
+    input.push('\n');
 
     let button_a = button_a.captures_iter(&input).map(|c| {
         let (_, [x, y]) = c.extract();
-        (x.parse::<u64>().unwrap(), y.parse::<u64>().unwrap())
+        Position::new(x.parse::<u64>().unwrap(), y.parse::<u64>().unwrap())
     });
 
     let button_b = button_b.captures_iter(&input).map(|c| {
         let (_, [x, y]) = c.extract();
-        (x.parse::<u64>().unwrap(), y.parse::<u64>().unwrap())
+        Position::new(x.parse::<u64>().unwrap(), y.parse::<u64>().unwrap())
     });
 
     let prizes = prizes.captures_iter(&input).map(|c| {
         let (_, [x, y]) = c.extract();
-        (x.parse::<u64>().unwrap(), y.parse::<u64>().unwrap())
+        Position::new(x.parse::<u64>().unwrap(), y.parse::<u64>().unwrap())
     });
 
     button_a
         .zip(button_b)
         .zip(prizes)
-        .map(|((a, b), prize)| Machine {
-            a,
-            b,
-            prize,
-            a_efficency: efficiency(a, 3),
-            b_efficency: efficiency(b, 1),
-        })
+        .map(|((a, b), prize)| Machine { a, b, prize })
         .collect()
+}
+
+fn spawn(pool: ThreadPool, tx: mpsc::Sender<Option<u64>>, input: Vec<Machine>) {
+    println!("Spawning for: {}", input.len());
+    for machine in input {
+        let tx = tx.clone();
+        pool.execute(move || {
+            tx.send(machine.calculate()).unwrap();
+        });
+    }
 }
 
 pub fn parta() -> u64 {
     let machines = read_input();
-    let pool = ThreadPool::new(24);
 
-    let (tx, rx) = mpsc::channel();
-    for machine in machines {
-        let tx = tx.clone();
-        pool.execute(move || {
-            tx.send(machine.calculate((0, 0), 0, 0, 0))
-                .expect("channel will be there waiting for the pool");
-        });
-    }
+    machines.iter().filter_map(|x| x.calculate()).sum()
+    // let pool = ThreadPool::new(24);
 
-    rx.iter().filter_map(|x| x).sum()
+    // let (tx, rx) = mpsc::channel();
+    // spawn(pool, tx, machines);
+
+    // let mut total = 0;
+    // for x in rx.iter() {
+    //     println!("Got: {:?}", x);
+    //     if let Some(x) = x {
+    //         total += x;
+    //     } else {
+    //         break;
+    //     }
+    // }
+
+    // println!("Total: {}", total);
+
+    // total
 }
-pub fn partb() -> usize {
-    0
+pub fn partb() -> u64 {
+    let machines = read_input();
+    println!("Machines: {:?}", machines.len());
+
+    let result = machines
+        .iter()
+        .map(|machine| Machine {
+            a: machine.a,
+            b: machine.b,
+            prize: Position::new(
+                machine.prize.x + 10000000000000,
+                machine.prize.y + 10000000000000,
+            ),
+        })
+        .filter_map(|x| x.calculate2())
+        .sum();
+
+    println!("Result: {}", result);
+    result
 }
 
 #[cfg(test)]
@@ -137,11 +210,11 @@ mod tests {
 
     #[test]
     fn test_parta() {
-        assert_eq!(0, parta());
+        // assert_eq!(37128, parta());
     }
 
     #[test]
     fn test_partb() {
-        assert_eq!(0, partb());
+        // assert_eq!(2, partb());
     }
 }
